a0315ced90f013fd306915dd18a5c45c09170d9a
neozenweb <37545009+neozenweb@users.noreply.github.com>
2018-05-17 09:02:35 -0400

    Memory Game initial setup. The game is now functional. Move counter is also coded.

--- js/app.js
+++ js/app.js
@@ -36,3 +36,72 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+  
+var firstCard;
+var openCards = document.getElementsByClassName("card open show");
+var cardArr;
+cardArr = document.getElementsByTagName("li");
+for(var ct=0;ct<cardArr.length;ct++)
+    {
+        cardArr[ct].addEventListener("click",clickCard);
+    }
+
+function clickCard() {
+    alert(this.className);
+   
+    checkMatch(this);
+}
+
+function checkMatch(matchCard)
+{
+    
+  
+    alert("there are " + openCards.length + " open cards now");
+    
+    switch(openCards.length % 2)
+        {
+        
+           case 1: 
+
+                alert("Already opened card is " + firstCard.getElementsByTagName('i')[0].className);
+                alert("Card clicked is  " + matchCard.getElementsByTagName('i')[0].className);
+
+                if(firstCard.getElementsByTagName('i')[0].className == matchCard.getElementsByTagName('i')[0].className)
+                    {
+                        alert("The symbol in both cards is " + matchCard.getElementsByTagName('i')[0].className);
+                         firstCard.className = 'card open show';
+                       matchCard.className = 'card open show';
+
+
+                    }
+                else
+                    {
+                       firstCard.className = 'card';
+                       matchCard.className = 'card';
+                         incrementMoves();
+                    }
+                  
+                
+                 break;
+            case 0:
+                alert("inside the length 0");
+                 matchCard.className = "card open show";
+                firstCard = matchCard;
+                break;
+        
+           
+    
+        }
+    
+    
+}
+
+function incrementMoves()
+{
+    
+    var movesctr = parseInt(document.getElementsByClassName("moves")[0].innerHTML);
+   
+   if(movesctr >= 0)
+        document.getElementsByClassName("moves")[0].innerHTML = movesctr + 1;
+   
+}
